80529e771de8e4186c2b5f9e15f0d883b916e306
Jean Mayard <sanctuarygames30@gmail.com>
2022-12-07 19:25:17 -0500

    updated controllers

---
+++ controllers/star.js
@@ -0,0 +1,102 @@
+const express = require('express') // bring this in so we can make our router
+const Fruit = require('../models/star')
+
+
+/////
+// Create Router  variable to attach rooutes
+/////
+
+const router = express.Router() // router will have all routes attached to it
+
+
+//////////////////////////////////////////////
+//////// Actual Routes
+///////////////////////////////////////////////
+
+// Authorization Middleware
+router.use((req, res, next) => {
+    console.log(req.session);
+    if (req.session.loggedIn) {
+      next();
+    } else {
+      res.redirect("/user/login");
+    }
+  });
+  
+// router.get('/seed', (req, res) => {
+// })
+
+router.get('/', (req, res) => {
+
+    // Get all stars from mongo and send them back
+    Star.find({ username: req.session.username } )
+    .then((stars) => {
+        // res.json(fruits)
+        res.render('stars/index.ejs', { stars, user:req.session.username });
+    })
+    .catch(err => console.log(err))
+
+})
+
+router.get('/new', (req, res) => {
+    res.render('stars/new.ejs')
+})
+
+// create route
+router.post("/", (req, res) => {
+    // check if the deadOrAlive property should be true or false
+    req.body.deadOrAlive = req.body.deadOrAlive === "on" ? true : false;
+    // add username to req.body to track related user
+    req.body.username = req.session.username
+    // create the new star
+    Star.create(req.body, (err, star) => {
+      // redirect the user back to the main stars page after fruit created
+      res.redirect("/stars");
+    });
+  });
+
+router.get('/:id/edit', (req, res) => {
+
+    const id = req.params.id
+    // Find the star and send it to the edit.ejs  to prepopulate the form
+    Star.findById(id, (err, foundStar) => {
+        // res.json(foundStar)
+        res.render('stars/edit.ejs', { star: foundStar })
+    })
+})
+
+router.put('/:id', (req, res) => {
+    
+    req.body.deadOrAlive = req.body.deadOrAlive === 'on' ? true : false
+
+    Star.findByIdAndUpdate(req.params.id, req.body, {new: true},(err, updatedStar) => {
+        console.log(updatedStar)
+
+        res.redirect(`/stars/${req.params.id}`)
+        
+    })
+})
+
+router.get('/:id', (req, res)=>{
+
+    // Go and get star from the database
+    Star.findById(req.params.id)
+    .then((star)=> {
+        res.render('stars/show.ejs', {star})
+    })
+})
+
+router.delete('/:id', async (req, res) => {
+    // Method 3 async await
+
+    const deletedStar = await Star.findByIdAndDelete(req.params.id)
+
+    if(deletedStar){
+        res.redirect('/stars/')
+    }
+})
+
+/////////////
+///// export this router to use in other files
+//////////////
+module.exports = router
--- server.js
+++ server.js
@@ -1,38 +1,41 @@
-//import
-require("dotenv").config()
-const express = require("express")
-const morgan = require("morgan")
-const methodOverride = require("method-override")
-const mongoose = require("mongoose")
+equire("dotenv").config()  // Load env variables
+const express = require('express') // bring in express to make our app
+const morgan = require('morgan') // nice logger for our request
+const methodOverride = require('method-override') // allows us to override post request from our ejs/forms
+const PORT = process.env.PORT 
+const StarRouter = require('./controllers/star')
+const UserRouter = require('./controllers/User');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const { response } = require("express")
 
-
-// create express app
 const app = express()
 
+//////////////////////////////////////////////
+//////// Middlewares
+///////////////////////////////////////////////
 
-// establish mongo connection
-mongoose.set('strictQuery', true)
-mongoose.connect(process.env.DATABASEURL)
-
-// mongoose connection events
-mongoose.connection
-.on("open", () => console.log("Connected to Mongoose"))
-.on("close", () => console.log("Disconnected from Mongoose"))
-.on("error", (error) => console.log(error))
-
-
-//register middlware
-app.use(morgan("dev"))
-app.use("static", express.static("public"))
-app.use(express.urlencoded({extended: true}))
-app.use(methodOverride("_method"))
-
-// Routes and Routers
-app.get("/", (req, rest) => {
-    rest.send("<h1>Server is Working</h1>")
-})
+app.use(morgan("tiny")); //logging
+app.use(methodOverride("_method")); // override for put and delete requests from forms
+app.use(express.urlencoded({ extended: true })); // parse urlencoded request bodies
+app.use(express.static("public")); // serve files from public statically
+app.use(session({
+    secret: process.env.SECRET,
+    store: MongoStore.create({mongoUrl: process.env.DATABASE_URL}),
+    saveUninitialized: true,
+    resave: false,
+  }))
 
 
-// start server 
-const PORT = process.env.PORT
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+// app.get('/', homeRoutes)
+// app.get('/store', storeRoutes)
+// app.get('/user', userRoutes)
+app.use('/stars', StarRouter)
+app.use('/user', UserRouter)
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+
+});
+
+app.listen(PORT, ()=> console.log(`Server is workiong on: ${PORT}`))
